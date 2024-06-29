84dc12b555ca1182ce363694949b1fced2d8c489
LAPTOP-2C4PL9J8\jingt <10121012tt@gmail.com>
2024-06-30 08:33:57 +0900

    3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/noteapp-with-react/'
+  // base: '/noteapp-with-react/'
+  base: process.env.GITHUB_PAGES  // この行を追加
+        ? "noteapp-with-react"            // この行を追加
+        : "./",                     // この行を追加
 })
